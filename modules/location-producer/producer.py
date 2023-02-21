70310e0e6b2359c69003486d5fd5ca2b4a6dd7a2
Nikos Kordis <nikoskordis@gmail.com>
2023-02-21 12:16:29 +0100

    Improve the location-producer

--- modules/location-producer/producer.py
+++ modules/location-producer/producer.py
@@ -8,8 +8,8 @@ from kafka import KafkaProducer
 producer = KafkaProducer(bootstrap_servers='kafka-service:9092')
 topic = 'location'
 
-start_time = time.time()
-while (time.time() - start_time) < 300:  # Run for 5 minutes
+
+while True:
     latitude = round(random.uniform(-90, 90), 4)
     longitude = round(random.uniform(-180, 180), 4)
     data = {'user_id': 1, 'latitude': latitude, 'longitude': longitude}
@@ -17,7 +17,7 @@ while (time.time() - start_time) < 300:  # Run for 5 minutes
     producer.send(topic, message)
     sys.stdout.write(f"Sent message to Kafka: {message}\n")
     sys.stdout.flush()
-    time.sleep(10)
+    time.sleep(20)
 
 sys.stdout.write("Finished sending messages to Kafka\n")
 sys.stdout.flush()
